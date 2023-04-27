9c9614d581c345a88e96a4f3e15509e0f445a3d6
Ewen <ewen1.malenfant@epitech.eu>
2023-04-27 11:18:58 -0400

    [ADD] get_all_conv

--- new_server/server.js
+++ new_server/server.js
@@ -9,9 +9,11 @@ app.get('/', (req, res) => {
 })
 const arr = []
 var room = ""
+var username
 
 axios.get('http://localhost:3001/GetUser')
     .then(function (response) {
+        username = response.data
         arr.push(response.data)
         axios.get('http://localhost:3001/GetFriend')
             .then(function (response) {
@@ -50,6 +52,21 @@ axios.get('http://localhost:3001/send_message')
     }).catch(function (error) {
         console.log("ok")
     })
+
+
+app.get('/get_all_conv', function (req, res) {
+    query.get_friends(username, function (dt, err) {
+        if (err) {
+            // error handling code goes here
+            console.log("ERROR : ", err);
+        } else {
+            console.log(dt)
+            res.send(dt)
+
+        }
+
+    });
+})
 // app.set('/receive_message/', function (req, res) {
 //     req.query.Room === room
 //     console.log(Room)
