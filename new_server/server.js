f030b55c84b49bd67806d83fb81aa7b8d43ce6e3
Ewen <ewen1.malenfant@epitech.eu>
2023-04-27 11:04:26 -0400

    [ADD] new serv

---
+++ new_server/server.js
@@ -0,0 +1,60 @@
+const query = require('./query.js');
+const express = require('express')
+const axios = require('axios');
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+const arr = []
+var room = ""
+
+axios.get('http://localhost:3001/GetUser')
+    .then(function (response) {
+        arr.push(response.data)
+        axios.get('http://localhost:3001/GetFriend')
+            .then(function (response) {
+                arr.push(response.data)
+                arr.sort()
+                room = arr[0].toLowerCase() + '_' + arr[1].toLowerCase()
+                query.connection(room)
+
+            })
+    }).catch(function (error) {
+        console.log("ok")
+    })
+
+app.get('/room', function (req, res) {
+    res.send(room)
+    app.get('/conv', function (req, res) {
+        query.get_conv(room, function (dt, err) {
+            if (err) {
+                // error handling code goes here
+                console.log("ERROR : ", err);
+            } else {
+                console.log(dt)
+                res.send(dt)
+
+            }
+
+        });
+    })
+})
+
+axios.get('http://localhost:3001/send_message')
+    .then(function (response) {
+        console.log(response.data[2])
+        query.save_mess(response.data[2], response.data[0], response.data[1])
+
+    }).catch(function (error) {
+        console.log("ok")
+    })
+// app.set('/receive_message/', function (req, res) {
+//     req.query.Room === room
+//     console.log(Room)
+//     res.send(Room)
+// })
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
